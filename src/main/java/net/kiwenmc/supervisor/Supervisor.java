1e8c646e33c1875dd260430a22120c68b19c4dac
Dallin Hoyt <dhhoyt2002@gmail.com>
2023-09-05 00:08:40 -0600

    inital commit

---
+++ src/main/java/net/kiwenmc/supervisor/Supervisor.java
@@ -0,0 +1,23 @@
+package net.kiwenmc.supervisor;
+
+import com.google.inject.Inject;
+import com.velocitypowered.api.plugin.Plugin;
+import com.velocitypowered.api.proxy.ProxyServer;
+import org.slf4j.Logger;
+
+
+@Plugin(id = "kiwensupervisor", name = "Kiwen Supervisor", version = "0.1.0-SNAPSHOT",
+        url = "https://kiwenmc.net", description = "Manages backend servers for Kiwen servers", authors = {"Boxman"})
+public class Supervisor {
+
+    private final ProxyServer server;
+    private final Logger logger;
+
+    @Inject
+    public Supervisor(ProxyServer server, Logger logger) {
+        this.server = server;
+        this.logger = logger;
+
+        logger.info("Hello there! I made my first plugin with Velocity.");
+    }
+}
